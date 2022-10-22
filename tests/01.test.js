da7486823fdb3ee2fd616d65df97e707a027db1c
j048 <115425518+j048@users.noreply.github.com>
2022-10-21 21:36:40 -0500

    proceso 1

--- tests/01.test.js
+++ tests/01.test.js
@@ -1,6 +1,6 @@
-const soloNumeros = require('../01.js');hla 
-describe('soloNumeros', function() {
-  it('should return [1, 2] for [1, "Henry", 2]', function() {
+const soloNumeros = require('../01.js');hola 
+describe('soloNumeros', function() {soy jose david pimienta
+  it('should return [1, 2] for [1, "Henry", 2]', function(estudiante de henry) {
       expect(soloNumeros([1, 'Henry', 2])).toStrictEqual([1, 2]);
   });
   it('should return [1, 3, 2] for [1, "Soy", 3, "Henry", 2]', function() {
